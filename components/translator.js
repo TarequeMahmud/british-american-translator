955b1ae5abf3adb768c7cf1b614acba46b744456
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-23 11:35:35 +0600

    feat: add functionality to validate user input.

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,17 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
-
+  validate(text, locale) {
+    const locales = ["american-to-british", "british-to-american"];
+    if (!text || !locale) return { error: "Required field(s) missing" };
+    if (text === "") return { error: "No text to translate" };
+    if (locales.indexOf(locale.toLowerCase()) === -1)
+      return { error: "Invalid value for locale field" };
+    return true;
+  }
 }
 
 module.exports = Translator;
