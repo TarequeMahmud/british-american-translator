ff1b26305bcdecf01f58d6653f115cd6e45f10a0
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-23 14:37:39 +0600

    test: add unit test for translating titles from american to british.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,24 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+const translator = new Translator();
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  suite("Test American to British Title translation", () => {
+    test("Translate Dr. Grosh will see you now. to British English", (done) => {
+      const text = "Dr. Grosh will see you now.";
+      const result = "Dr Grosh will see you now.";
+      assert.equal(translator.translate(text, "american-to-british"), result);
+      done();
+      //test ends at the following brackets
+    });
+    test("Translate 'No Mr. Bond, I expect you to die.' to British English", (done) => {
+      const text = "No Mr. Bond, I expect you to die.";
+      const result = "No Mr Bond, I expect you to die.";
+      assert.equal(translator.translate(text, "american-to-british"), result);
+      done();
+      //test ends at the following brackets
+    });
+  });
 });
