4ce6f5862456f0702c4d9008f4e1ddf07501786e
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-23 14:36:11 +0600

    feat: add functionality to translate titles from american to british.

--- components/translator.js
+++ components/translator.js
@@ -4,13 +4,38 @@ const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
 
 class Translator {
+  constructor() {
+    this.locales = ["american-to-british", "british-to-american"];
+  }
+
   validate(text, locale) {
-    const locales = ["american-to-british", "british-to-american"];
     if (!text || !locale) return { error: "Required field(s) missing" };
     if (text === "") return { error: "No text to translate" };
-    if (locales.indexOf(locale.toLowerCase()) === -1)
+    if (this.locales.indexOf(locale.toLowerCase()) === -1)
       return { error: "Invalid value for locale field" };
     return true;
+  }
+  translate(text, locale) {
+    const isValidate = this.validate(text, locale);
+    if (isValidate !== true) return isValidate;
+
+    if (locale.toLowerCase() === this.locales[0]) {
+      const matches = Object.keys(americanToBritishTitles).filter((value) =>
+        text.toLowerCase().includes(value)
+      );
+
+      if (matches)
+        matches.forEach((match) => {
+          const regex = new RegExp(match, "i");
+          text = text.replace(
+            regex,
+            americanToBritishTitles[match].charAt(0).toUpperCase() +
+              americanToBritishTitles[match].slice(1)
+          );
+        });
+
+      return text;
+    }
   }
 }
 
