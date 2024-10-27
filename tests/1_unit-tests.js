9e7ecfbfb7e250a48dc92ab0e4d1ca69297090cf
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-27 11:09:54 +0600

    test: add unit test for translating whole word from american to british.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,6 +3,36 @@ const assert = chai.assert;
 
 const Translator = require("../components/translator.js");
 const translator = new Translator();
+const americanToBritish = [
+  {
+    text: "Mangoes are my favorite fruit.",
+    translate: "Mangoes are my favourite fruit.",
+  },
+  {
+    text: "I ate yogurt for breakfast.",
+    translate: "I ate yoghurt for breakfast.",
+  },
+  {
+    text: "Can you toss this in the trashcan for me?",
+    translate: "Can you toss this in the bin for me?",
+  },
+  {
+    text: "We had a party at my friend's condo.",
+    translate: "We had a party at my friend's flat.",
+  },
+  {
+    text: "The parking lot was full.",
+    translate: "The car park was full.",
+  },
+  {
+    text: "Like a high tech Rube Goldberg machine.",
+    translate: "Like a high tech Heath Robinson device.",
+  },
+  {
+    text: "To play hooky means to skip class or work.",
+    translate: "To bunk off means to skip class or work.",
+  },
+];
 
 suite("Unit Tests", () => {
   suite("Test American to British Title translation", () => {
@@ -19,6 +49,18 @@ suite("Unit Tests", () => {
       assert.equal(translator.translate(text, "american-to-british"), result);
       done();
       //test ends at the following brackets
+    });
+  });
+  suite("Test American to British word translation", () => {
+    americanToBritish.forEach((text) => {
+      test("Translate Mangoes are my favorite fruit. to British English", (done) => {
+        assert.equal(
+          translator.translate(text.text, "american-to-british"),
+          text.translate
+        );
+        done();
+        //test ends at the following brackets
+      });
     });
   });
 });
