f02703c7eb1e7e26dd5685b44979eb8db3cbf01c
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-28 02:09:28 +0600

    feat: add functional test for the POST route for the translator

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,120 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+let Translator = require("../components/translator.js");
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {
+  suite(
+    "POST /api/translate given british/american text => responds with translated text",
+    () => {
+      test("Translation with text and locale fields", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/translate")
+          .send({
+            text: "Mangoes are my favorite fruit.",
+            locale: "american-to-british",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
 
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(
+              res.body.translation,
+              'Mangoes are my <span class="highlight">favourite</span> fruit.'
+            );
+            done();
+          });
+      });
+      test("Translation with text and invalid locale field:", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/translate")
+          .send({
+            text: "Mangoes are my favorite fruit.",
+            locale: "amerigdcan-to-british",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.error, "Invalid value for locale field");
+            done();
+          });
+      });
+      test("Translation with missing text field", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/translate")
+          .send({
+            locale: "american-to-british",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.error, "Required field(s) missing");
+            done();
+          });
+      });
+      test("Translation with missing locale field", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/translate")
+          .send({
+            text: "Mangoes are my favorite fruit.",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.error, "Required field(s) missing");
+
+            done();
+          });
+      });
+      test("Translation with empty text", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/translate")
+          .send({
+            text: "",
+            locale: "american-to-british",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.error, "No text to translate");
+            done();
+          });
+      });
+      test("Translation with text that needs no translation", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/translate")
+          .send({
+            text: "Paracetamol takes up to an hour to work.",
+            locale: "american-to-british",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.translation, "Everything looks good to me!");
+            done();
+          });
+      });
+    }
+  );
 });
