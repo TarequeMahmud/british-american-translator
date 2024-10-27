8f35623147a530474c2fe3108b01ef9253f13967
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-28 01:48:49 +0600

    feat: add POST route for translator

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,29 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+    console.log(req.body);
+
+    const isValidate = translator.validate(text, locale);
+    if (isValidate !== true) return res.json(isValidate);
+
+    const translation = translator.translate(
+      text,
+      locale,
+      translator.highlightText
+    );
+    if (text === translation)
+      return res.json({
+        text: text,
+        translation: "Everything looks good to me!",
+      });
+    console.log(translation);
+
+    return res.json({ text: text, translation: translation });
+  });
 };
